aa5f624578e48f8fe87fc301365b33452d755ca0
Olga Lyubinets <olga.lyubinets@Olgas-MBP.lan>
2018-11-18 22:05:56 +0000

    Updates

---
+++ precache-manifest.d6c81dc068a1298c996afc15e212451d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/public/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/public/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/public/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/public/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "c3ea8d972d62e65a19a9",
+    "url": "/public/static/js/runtime~main.c3ea8d97.js"
+  },
+  {
+    "revision": "846b106898bb58985391",
+    "url": "/public/static/js/main.846b1068.chunk.js"
+  },
+  {
+    "revision": "1e2840f12f530808d605",
+    "url": "/public/static/js/1.1e2840f1.chunk.js"
+  },
+  {
+    "revision": "846b106898bb58985391",
+    "url": "/public/static/css/main.9c19c813.chunk.css"
+  },
+  {
+    "revision": "1e2840f12f530808d605",
+    "url": "/public/static/css/1.42158650.chunk.css"
+  },
+  {
+    "revision": "84507dd2aa6e7a626202b3920a0527b7",
+    "url": "/public/index.html"
+  }
+];
