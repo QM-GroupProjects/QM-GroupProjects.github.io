ea25f9d99407c99f8b8d6dc06359490c347132c0
Olga Lyubinets <olga.lyubinets@Olgas-MBP.lan>
2018-11-18 21:30:19 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a871ab6763e882575ab46a9e7a47cfa9.js"
+  "/takemebythehand-react/precache-manifest.32561681a8ceeeff9039c4c6ab8c48b2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/takemebythehand-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
